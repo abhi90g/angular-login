4f97c5006c7bb3c09ae070ed5e7fa35b964e0ca9
Abhinav Gupta <abhinavgupta@Abhinavs-MBP.attlocal.net>
2021-06-02 15:19:45 -0500

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,101 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {
+  TestBed,
+  ComponentFixture,
+  tick,
+  fakeAsync,
+  waitForAsync,
+  ComponentFixtureAutoDetect,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [AppComponent],
+      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+      });
+
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'vocera-assessment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('vocera-assessment');
+  it(`should have as title 'angular login'`, () => {
+    expect(component.title).toEqual('angular-login');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should show message when clicked on forgot password', fakeAsync(() => {
+    const buttonElem = fixture.nativeElement.querySelector('.forgotpass');
+    const message = fixture.nativeElement.querySelector('.messages');
+
+    spyOn(component, 'forgot'); // call the forgot method
+    buttonElem.click(); //click button
+    component.showMessage = true;
+    component.messageText = 'Forgot password clicked!';
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('vocera-assessment app is running!');
-  });
+
+    expect(component.forgot).toHaveBeenCalled(); //forgot password clicked
+    expect(message.textContent).toEqual('Forgot password clicked!'); //message show in dom
+  }));
+
+  it('should show message when clicked on sign up', fakeAsync(() => {
+    const buttonElem = fixture.debugElement.query(By.css('a'));
+    const message = fixture.nativeElement.querySelector('.messages');
+    spyOn(component, 'signup');
+    buttonElem.triggerEventHandler('click', null);
+    component.showMessage = true;
+    component.messageText = 'Sign up clicked!';
+    fixture.detectChanges();
+
+    expect(component.signup).toHaveBeenCalled(); //signup clicked
+    expect(message.textContent).toEqual('Sign up clicked!'); //message show in dom
+  }));
+
+  it('should show message when clicked on login button without username or password', fakeAsync(() => {
+    // const loginBtn = fixture.nativeElement.querySelector('.btn')
+    const loginBtn = fixture.debugElement.query(By.css('.btn'));
+    const message = fixture.nativeElement.querySelector('.messages');
+    component.ngOnInit();
+    loginBtn.triggerEventHandler('click', null);
+    component.showMessage = true;
+    component.messageText = 'You must enter a username or password';
+
+    fixture.detectChanges();
+    expect(message.textContent).toEqual(
+      'You must enter a username or password'
+    );
+  }));
+
+  it('should show json when clicked on login button WITH username or password', fakeAsync(() => {
+    const loginBtn = fixture.debugElement.query(By.css('.btn'));
+    const message = fixture.nativeElement.querySelector('.messages');
+    component.ngOnInit();
+    // setting form values
+    component.loginForm.controls['username'].setValue('user');
+    component.loginForm.controls['password'].setValue('pass');
+    component.loginForm.controls['rememberMe'].setValue(true);
+
+    const expectedObj = {
+      username: 'user',
+      password: 'pass',
+      rememberMe: true,
+    };
+    loginBtn.triggerEventHandler('click', null);
+    component.showLogindata = true;
+    component.showMessage = false;
+
+    fixture.detectChanges();
+    expect(JSON.parse(message.textContent)).toEqual(expectedObj);
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'vocera-assessment';
+export class AppComponent implements OnInit {
+  title = 'angular-login';
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    rememberMe: new FormControl(false)
+  });
+  showLogindata: Boolean = false;
+  showMessage: Boolean = false
+  messageText: string = ''
+
+  ngOnInit() {
+    this.showMessage = false;
+    this.messageText = '';
+    this.showLogindata = false;
+  }
+
+  forgot() {
+    this.showMessage = true;
+    this.messageText = 'Forgot password clicked!'
+  }
+
+  signup() {
+    this.showMessage = true;
+    this.messageText = 'Sign up clicked!'
+  }
+
+  onSubmit() {
+    if (this.loginForm.value.username || this.loginForm.value.password) {
+      this.showLogindata = true
+      this.showMessage = false;
+      this.messageText = ''
+    } else {
+      this.showMessage = true;
+      this.messageText = 'You must enter a username or password'
+    }
+  }
 }
